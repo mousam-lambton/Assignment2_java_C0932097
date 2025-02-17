29a4f7aa5daebc82fb44b14f4609686e2d736f65
mousam-soliish <mousam@soliish.com>
2025-02-17 16:39:36 -0500

    Add getPostsByAuthorAge method to get posts by author age

--- src/main/java/assignment2/Blog.java
+++ src/main/java/assignment2/Blog.java
@@ -3,8 +3,11 @@ package assignment2;
 import lombok.Getter;
 import lombok.ToString;
 import lombok.EqualsAndHashCode;
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Getter
 @ToString
@@ -16,5 +19,19 @@ public class Blog {
     public Blog(List<BlogPost> posts, List<Person> contributors) {
         this.posts = posts != null ? new ArrayList<>(posts) : new ArrayList<>();
         this.contributors = contributors != null ? new ArrayList<>(contributors) : new ArrayList<>();
+    }
+
+    public List<String> getPostsByAuthorAge(Integer age) {
+        // Get author IDs of contributors with the given age
+        Set<String> authorIds = contributors.stream()
+                .filter(person -> person.getAge().equals(age))
+                .map(Person::getId)
+                .collect(Collectors.toSet());
+
+        // Get IDs of posts by authors with the given age from above
+        return posts.stream()
+                .filter(post -> authorIds.contains(post.getAuthorId()))
+                .map(BlogPost::getId)
+                .collect(Collectors.toList());
     }
 }
